77b1096d4109102a9b0cbc6f163f6107b4c173f4
eugene <eugenefromrus@gmail.com>
2021-12-11 01:04:28 +1000

    Правка якоря и topSwiper

--- components/ui/UiSrollUp.js
+++ components/ui/UiSrollUp.js
@@ -1,11 +1,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import logoBlack from '@/base/icon-arrow-top.svg';
+import React, {useEffect, useState} from 'react';
 
 export default function UiSrollUp() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+      console.log(offset);
+    };
+  }, []);
+
+  console.log(offset);
   return (
     <Link href="#__next">
-      <a className="ui-scroll-up">
+      <a className={offset > 600 ? "ui-scroll-up" : "ui-scroll-up ui-scroll-up_disabled"}>
         <div className="ui-scroll-up__img-wrapper">
           <Image src={logoBlack} alt="scroll up" objectFit="contain" layout="responsive" />
         </div>
