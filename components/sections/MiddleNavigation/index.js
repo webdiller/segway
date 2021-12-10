2d5cfc7cfa9294b420c3dd39939ffaeb58d3aa3d
eugene <eugenefromrus@gmail.com>
2021-12-10 22:18:34 +1000

    Проверка кнопки скачать мануал

--- components/sections/MiddleNavigation/index.js
+++ components/sections/MiddleNavigation/index.js
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
-import logo from "@/base/logo-black-large.svg";
-import profileImg from "@/base/profile-img.png";
+import logo from '@/base/logo-black-large.svg';
+import profileImg from '@/base/profile-img.png';
 
 /** Навигация с логотипом и профилем */
 export default function MiddleNavigation() {
@@ -11,29 +11,18 @@ export default function MiddleNavigation() {
       <div className="container middle-navigation__container">
         <Link href="/">
           <a className="middle-navigation__logo-link">
-            <Image
-              className="middle-navigation__logo-img"
-              src={logo}
-              alt="segway"
-              width={216}
-              height={63}
-            />
+            <Image className="middle-navigation__logo-img" src={logo} alt="segway" width={216} height={63} />
           </a>
         </Link>
 
         <div className="middle-navigation__director">
           <div className="middle-navigation__director-img-wrapper">
-            <Image
-              className="middle-navigation__director-img"
-              src={profileImg}
-              alt="JOHN SMITH"
-              width={67}
-              height={67}
-            />
+            <Image className="middle-navigation__director-img" src={profileImg} alt="JOHN SMITH" width={67} height={67} />
           </div>
 
           <div className="middle-navigation__director-items">
             <span className="middle-navigation__director-name">JOHN SMITH</span>
+            {/* <p className="middle-navigation__director-description">«Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod facilisi pharetra sed nulla massa. </p> */}
             <Link href="#">
               <a className="middle-navigation__director-link">
                 Ask a question <br /> to the director
