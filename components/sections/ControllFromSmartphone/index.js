cfa83c7f08f1beeec15c98e28bff5d6af671069e
eugene <eugenefromrus@gmail.com>
2021-12-17 14:39:50 +1000

    Правки в блоке controll from ur smartphone

--- components/sections/ControllFromSmartphone/index.js
+++ components/sections/ControllFromSmartphone/index.js
@@ -1,21 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
+import Image from 'next/image';
 
-import smartphoneDesktop from "@/base/controll-from-smartphone-payments.png";
+import smartphoneDesktop from '@/base/controll-from-smartphone-payments.png';
 
-import ControllFromSmartphoneIcon_1_white from "@/base/controll-from-smartphone-1-white.svg";
-import ControllFromSmartphoneIcon_2_white from "@/base/controll-from-smartphone-2-white.svg";
-import ControllFromSmartphoneIcon_3_white from "@/base/controll-from-smartphone-3-white.svg";
-import ControllFromSmartphoneIcon_4_white from "@/base/controll-from-smartphone-4-white.svg";
-import ControllFromSmartphoneIcon_5_white from "@/base/controll-from-smartphone-5-white.svg";
-import ControllFromSmartphoneIcon_6_white from "@/base/controll-from-smartphone-6-white.svg";
-
-import ControllFromSmartphoneIcon_1_color from "@/base/controll-from-smartphone-1-color.svg";
-import ControllFromSmartphoneIcon_2_color from "@/base/controll-from-smartphone-2-color.svg";
-import ControllFromSmartphoneIcon_3_color from "@/base/controll-from-smartphone-3-color.svg";
-import ControllFromSmartphoneIcon_4_color from "@/base/controll-from-smartphone-4-color.svg";
-import ControllFromSmartphoneIcon_5_color from "@/base/controll-from-smartphone-5-color.svg";
-import ControllFromSmartphoneIcon_6_color from "@/base/controll-from-smartphone-6-color.svg";
+const ControllItem = ({imgPathBorder, imgPathFill, imgPathMobile, text} = {}) => {
+  return (
+    <div className="controll-from-smartphone__item">
+      <div className="controll-from-smartphone__item-img-wrapper">
+        <img loading="lazy" src={imgPathBorder} alt="icon" className="controll-from-smartphone__item-img controll-from-smartphone__item-img_border" />
+        <img loading="lazy" src={imgPathFill} alt="icon" className="controll-from-smartphone__item-img controll-from-smartphone__item-img_fill" />
+        <img loading="lazy" src={imgPathMobile} alt="icon" className="controll-from-smartphone__item-img controll-from-smartphone__item-img_mobile" />
+      </div>
+      <p className="controll-from-smartphone__item-description">{text}</p>
+    </div>
+  );
+};
 
 export default function ControllFromSmartphone() {
   return (
@@ -29,122 +28,40 @@ export default function ControllFromSmartphone() {
             </span>
           </p>
           <p className="text controll-from-smartphone__text">
-            You can easily control your Segway electric scooter with your
-            smartphone via Bluetooth. Not being the key to the kickscooter, it
-            will give a signal in case of an attempt to steal. Check the current
-            speed, data on the status of the device’s systems or set the speed
-            limit with a few touches. And the skating training program is
-            indispensable for beginners.
+            You can easily control your Segway electric scooter with your smartphone via Bluetooth. Not being the
+            <br />
+            key to the kickscooter, it will give a signal in case of an attempt to steal. Check the current speed, data <br />
+            on the status of the device’s systems or set the speed limit with a few touches. <br />
+            And the skating training program is indispensable for beginners.
           </p>
         </div>
 
         <div className="controll-from-smartphone__bottom">
           <div className="controll-from-smartphone__bottom-left">
             <div className="controll-from-smartphone__items">
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_1_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_1_color} alt="icon" />
-                </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-1-border.svg" imgPathFill="./controll-from-smartphone-1-fill.svg" imgPathMobile="./control-1-border-mobile.svg" text="Anti-theft protection" />
 
-                <p className="controll-from-smartphone__item-description">
-                  Anti-theft protection
-                </p>
-              </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-2-border.svg" imgPathFill="./controll-from-smartphone-2-fill.svg" imgPathMobile="./control-2-border-mobile.svg" text="Dashboard" />
 
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_2_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_2_color} alt="icon" />
-                </div>
-                <p className="controll-from-smartphone__item-description">
-                  Dashboard
-                </p>
-              </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-3-border.svg" imgPathFill="./controll-from-smartphone-3-fill.svg" imgPathMobile="./control-3-border-mobile.svg" text="Firmware upgrade" />
 
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_3_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_3_color} alt="icon" />
-                </div>
-                <p className="controll-from-smartphone__item-description">
-                  Firmware upgrade
-                </p>
-              </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-4-border.svg" imgPathFill="./controll-from-smartphone-4-fill.svg" imgPathMobile="./control-4-border-mobile.svg" text="Device status" />
 
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_4_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_4_color} alt="icon" />
-                </div>
-                <p className="controll-from-smartphone__item-description">
-                  Device status
-                </p>
-              </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-5-border.svg" imgPathFill="./controll-from-smartphone-5-fill.svg" imgPathMobile="./control-5-border-mobile.svg" text="Settings and restrictions" />
 
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_5_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_5_color} alt="icon" />
-                </div>
-                <p className="controll-from-smartphone__item-description">
-                  Settings and restrictions
-                </p>
-              </div>
-
-              <div className="controll-from-smartphone__item">
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_white">
-                  <Image src={ControllFromSmartphoneIcon_6_white} alt="icon" />
-                </div>
-                <div className="controll-from-smartphone__item-icon-wrapper controll-from-smartphone__item-icon-wrapper_color">
-                  <Image src={ControllFromSmartphoneIcon_6_color} alt="icon" />
-                </div>
-                <p className="controll-from-smartphone__item-description">
-                  Training program
-                </p>
-              </div>
+              <ControllItem imgPathBorder="./controll-from-smartphone-6-border.svg" imgPathFill="./controll-from-smartphone-6-fill.svg" imgPathMobile="./control-6-border-mobile.svg" text="Training programs" />
             </div>
 
             <div className="controll-from-smartphone__payments-img-wrapper">
-              <Image
-                className="controll-from-smartphone__payments-img"
-                src={smartphoneDesktop}
-                alt="Smart battery Management system (Smart-BMS)"
-                width={844}
-                height={123}
-                layout="responsive"
-              />
+              <Image className="controll-from-smartphone__payments-img" src={smartphoneDesktop} alt="Smart battery Management system (Smart-BMS)" width={844} height={123} layout="responsive" />
             </div>
           </div>
           <div className="controll-from-smartphone__bottom-right">
             <picture className="controll-from-smartphone__hand-picture">
-              <source
-                srcSet="/controll-from-smartphone-hand-desktop.webp"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet="/controll-from-smartphone-hand-desktop.png"
-                media="(min-width: 768px)"
-              />
+              <source srcSet="/controll-from-smartphone-hand-desktop.webp" media="(min-width: 768px)" />
+              <source srcSet="/controll-from-smartphone-hand-desktop.png" media="(min-width: 768px)" />
               <source srcSet="/controll-from-smartphone-hand-mobile.webp" />
-              <img
-                width="762"
-                height="465"
-                className="controll-from-smartphone__hand-img"
-                src="/controll-from-smartphone-hand-mobile.jpg"
-                alt="Control from your smartphone"
-                loading="lazy"
-              />
+              <img width="762" height="465" className="controll-from-smartphone__hand-img" src="/controll-from-smartphone-hand-mobile.jpg" alt="Control from your smartphone" loading="lazy" />
             </picture>
           </div>
         </div>
