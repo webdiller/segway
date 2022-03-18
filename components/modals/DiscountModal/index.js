5323b5fcb801d70633e256210d62f73f685ed24e
eugene <eugenefromrus@gmail.com>
2022-03-18 20:36:08 +1000

    Рефакторинг анимации discount modal

--- components/modals/DiscountModal/index.js
+++ components/modals/DiscountModal/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import Image from 'next/image';
 import { setActive, setUserPhone } from 'store/slices/discountModalSlice';
 import ReactInputMask from 'react-input-mask';
@@ -66,55 +66,64 @@ export default function DiscountModal() {
   }, [success, dispatch])
 
   return (
-    <>
-      <motion.div
-        animate={{ backdropFilter: blur(10), }}
-        initial={{ backdropFilter: blur(0) }}
-        onClick={(e) => onClickWrapper(e)}
-        ref={elRef}
-        className={isActive ? 'discount-modal active' : 'discount-modal'}>
-        <motion.form
-          onDragEnd={(event, info) => {
-            const matchOffsetX = info.offset.x < -150 || info.offset.x > 150;
-            const matchOffsetY = info.offset.y < -150 || info.offset.y > 150;
-            if (matchOffsetX || matchOffsetY) dispatch(setActive(false))
-          }}
-          whileDrag={{ scale: 0.95 }}
-          drag
-          dragConstraints={{
-            top: -50,
-            left: -50,
-            right: 50,
-            bottom: 50,
-          }}
-          animate={{ top: 0, opacity: 1 }}
-          initial={{ top: -100, opacity: 0 }}
-          onSubmit={onSubmit}
-          className="discount-modal__wrapper">
-          <button type='button' onClick={setActiveModalHandler} className="discount-modal__close-btn">
-            <div className='discount-modal__close-btn-icon discount-modal__close-btn-icon_desktop'><Image src="/icon-close-white.svg" alt="icon-close" width="34" height="34" loading="lazy" /></div>
-            <div className='discount-modal__close-btn-icon discount-modal__close-btn-icon_mobile'><Image src="/icon-close-black.svg" alt="icon-close" width="34" height="34" loading="lazy" /></div>
-          </button>
-          <p className="discount-modal__title">5%</p>
-          <div className="discount-modal__content">
-            <p className="title discount-modal__subtitle">discount</p>
-            <p className="text discount-modal__description">Enter your phone number and our manager will call your back in 15 seconds</p>
-          </div>
-          <ReactInputMask
-            name="formFromOtherModelsPhone"
-            onChange={(e) => dispatch(setUserPhone(e.target.value))}
-            value={userPhone}
-            placeholder="+1 ___ ___ __ __"
-            mask="+1 999 999 99 99"
-            className='ui-input didnt-find-modal__input'
-            required
-          />
-          <button ref={btnRef} type='submit' className="ui-btn discount-modal__btn">
-            <span>Get a discount</span>
-          </button>
-        </motion.form>
-      </motion.div>
-      <div style={{ zIndex: loading || !readyToInteractiveWithModal ? 1000 : -1 }} className="discount-modal-locker"></div>
-    </>
+    <AnimatePresence>
+      {isActive && (
+        <>
+          <motion.div
+            animate={{ zIndex: 1000 }}
+            initial={{ zIndex: -1 }}
+            exit={{ opacity: 0 }}
+            onClick={(e) => onClickWrapper(e)}
+            ref={elRef}
+
+            className='discount-modal'>
+            <motion.form
+              drag
+              onDragEnd={(_, info) => {
+                const matchOffsetX = info.offset.x < -150 || info.offset.x > 150;
+                const matchOffsetY = info.offset.y < -150 || info.offset.y > 150;
+                if (matchOffsetX || matchOffsetY) dispatch(setActive(false))
+              }}
+              whileDrag={{ scale: 0.95 }}
+              dragConstraints={{
+                top: -50,
+                left: -50,
+                right: 50,
+                bottom: 50,
+              }}
+
+              animate={{ top: 0, opacity: 1, scale: 1 }}
+              initial={{ top: -20, opacity: 0, scale: .9 }}
+              exit={{ scale: .9 }}
+
+              onSubmit={onSubmit}
+              className="discount-modal__wrapper">
+              <div type='button' onClick={setActiveModalHandler} className="discount-modal__close-btn">
+                <div className='discount-modal__close-btn-icon discount-modal__close-btn-icon_desktop'><Image src="/icon-close-white.svg" alt="icon-close" width="34" height="34" loading="lazy" /></div>
+                <div className='discount-modal__close-btn-icon discount-modal__close-btn-icon_mobile'><Image src="/icon-close-black.svg" alt="icon-close" width="34" height="34" loading="lazy" /></div>
+              </div>
+              <p className="discount-modal__title">5%</p>
+              <div className="discount-modal__content">
+                <p className="title discount-modal__subtitle">discount</p>
+                <p className="text discount-modal__description">Enter your phone number and our manager will call your back in 15 seconds</p>
+              </div>
+              <ReactInputMask
+                name="formFromOtherModelsPhone"
+                onChange={(e) => dispatch(setUserPhone(e.target.value))}
+                value={userPhone}
+                placeholder="+1 ___ ___ __ __"
+                mask="+1 999 999 99 99"
+                className='ui-input didnt-find-modal__input'
+                required
+              />
+              <button ref={btnRef} type='submit' className="ui-btn discount-modal__btn">
+                <span>Get a discount</span>
+              </button>
+            </motion.form>
+          </motion.div>
+          <div style={{ zIndex: loading || !readyToInteractiveWithModal ? 1000 : -1 }} className="discount-modal-locker"></div>
+        </>
+      )}
+    </AnimatePresence>
   );
 }
